6180f96f14be72612f6bc45ac37070a5d314f187
linwenjun <linwenjun@163.com>
2020-08-19 08:10:25 +0000

    first commit

---
+++ src/test/java/tw/practice/basic/ExpenseServiceTest.java
@@ -0,0 +1,14 @@
+package tw.practice.basic;
+
+import org.junit.Test;
+
+public class ExpenseServiceTest {
+    @Test
+    public void should_return_internal_expense_type_if_project_is_internal() {
+        // given
+
+        // when
+
+        // then
+    }
+}
